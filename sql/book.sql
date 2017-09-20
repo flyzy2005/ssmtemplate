da6899c5bac6c82a118d1dddda255218314bc88c
flyzy2005 <flywf@whu.edu.cn>
2017-09-20 14:56:27 +0800

    remove the wrong comment in pom.xml and update file book.sql

--- sql/book.sql
+++ sql/book.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50528
 File Encoding         : 65001
 
-Date: 2017-07-19 17:23:41
+Date: 2017-09-20 14:53:08
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -24,13 +24,15 @@ CREATE TABLE `book` (
   `name` varchar(100) NOT NULL COMMENT '图书名称',
   `author` varchar(100) NOT NULL COMMENT '馆藏数量',
   `publisher` varchar(100) NOT NULL,
+  `large` blob,
+  `time` date DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1004 DEFAULT CHARSET=utf8 COMMENT='图书表';
 
 -- ----------------------------
 -- Records of book
 -- ----------------------------
-INSERT INTO `book` VALUES ('1000', 'Java程序设计', '程星会', 'fzt程');
-INSERT INTO `book` VALUES ('1001', '数据结构', '大师兄', 'fzt大师兄');
-INSERT INTO `book` VALUES ('1002', '设计模式', '会会', 'fzt会会');
-INSERT INTO `book` VALUES ('1003', '编译原理', '玉米会', 'fzt玉米');
+INSERT INTO `book` VALUES ('1000', 'Java程序设计', '程星会', 'fzt程', null, null);
+INSERT INTO `book` VALUES ('1001', '数据结构', '大师兄', 'fzt大师兄', null, null);
+INSERT INTO `book` VALUES ('1002', '设计模式', '会会', 'fzt会会', null, null);
+INSERT INTO `book` VALUES ('1003', '编译原理', '玉米会', 'fzt玉米', null, null);
